e541bbae4e4016ffade0260a9a3e33614467a869
João Pedro Abreu <jp.dellabianca@gmail.com>
2022-05-04 15:50:50 +0000

    Config mysql connect

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbHost = process.env.DB_HOST;
-const dbPassword = process.env.DB_PASSWORD;
+const dbName = "Flights";
+const dbUser = "root";
+const dbHost = "localhost";
+const dbPassword = "123";
 
 const sequelize = new Sequelize( dbName, dbUser, dbPassword, {
-    //passar os dados para o sequelize
     dialect: "mysql",
-    host: dbHost, //o host, neste caso estamos com um banco local
+    port: 5000,
+    host: dbHost, 
 } );
 
 export default sequelize; //exportar
