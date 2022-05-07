287e2febbbcc63431a63afa11b2e7934d4649d56
Joao Abreu <jp.dellabianca@.gmail.com>
2022-05-07 00:07:27 -0300

    Update handle error

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,17 +1,11 @@
 import CreateUserUseCase from "./CreateUserUseCase";
 import { Request, Response } from "express";
 
-
-export default class CreateUserController
-{
-    constructor ( private useCase: CreateUserUseCase )
-    {
-    }
-    handle ( request: Request, response: Response )
-    {
-        const { name, email, picture, password } = request.body;
-        console.log( name );
-        this.useCase.execute( { name, email, picture, password } );
-        return response.json( { foi: "foi" } );
-    }
+export default class CreateUserController {
+  constructor(private useCase: CreateUserUseCase) {}
+  handle(request: Request, response: Response) {
+    const { name, email, picture, password } = request.body;
+    this.useCase.execute({ name, email, picture, password });
+    return response.status(201);
+  }
 }
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,20 +1,19 @@
 import IUserRepository from "../../repositories/IUserRepository";
 
-interface IRequestCreateUser
-{
-    name: string;
-    email: string;
-    picture: string;
-    password: string;
+interface IRequestCreateUser {
+  name: string;
+  email: string;
+  picture: string;
+  password: string;
 }
 
-
-export default class CreateUserUseCase
-{
-    constructor ( private repository: IUserRepository ) { }
-    execute ( { name, email, picture, password }: IRequestCreateUser )
-    {
-        console.log( "foi use case" );
-        this.repository.createUser( { name, email, picture, password } );
+export default class CreateUserUseCase {
+  constructor(private repository: IUserRepository) {}
+  execute({ name, email, picture, password }: IRequestCreateUser) {
+    try {
+      this.repository.createUser({ name, email, picture, password });
+    } catch (error: any) {
+      throw new Error(error);
     }
+  }
 }
